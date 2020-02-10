7784f2dd46405c77f1bfd04e66465e9e5d5bce2c
Chak Kwong Lee <chakkl@uci.edu>
2020-02-09 20:49:41 -0800

    Added more website to ignore

--- launch.py
+++ launch.py
@@ -76,8 +76,6 @@ def outputResult():
     domain = sortDomain(domain)
     print("...")
     unique = countUniquePages(domain)
-    print(f"There are {unique} unique pages found.")
-    print(f"Longest page: {longestPage} -> {maxNumWords} words.")
     limit = 0
     stopWords = {"a", "about", "above", "after", "against", "again", "all", "am", "an", "and", "any", "are", "aren't",
                  "t", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by",
@@ -94,6 +92,8 @@ def outputResult():
                  "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's",
                  "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you",
                  "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"}
+    print(f"There are {unique} unique pages found.")
+    print(f"Longest page: {longestPage} -> {maxNumWords} words.")
     print("Common words: ")
     for word in sorted(commonWords.items(), key=lambda f: (-f[1], f[0]), reverse=True)[::-1]:
         if len(word[0]) > 2 and not isnum(word[0]) and word[0] not in stopWords:
